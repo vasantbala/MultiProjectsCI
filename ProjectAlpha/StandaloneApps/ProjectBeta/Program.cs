2a3625d8d76265a7199deea47d5fec89d81677fe
Vasant Balasubramanian <vasant.bala@gmail.com>
2023-06-24 12:21:18 -0500

    beta

--- ProjectAlpha/StandaloneApps/ProjectBeta/Program.cs
+++ ProjectAlpha/StandaloneApps/ProjectBeta/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Change to see if the CI build is triggered. another time, one more, another, one more
 //another, one more
+//Build beta
 Console.WriteLine("Hello, This is project Beta!");
