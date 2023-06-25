5fa2fc3348d405d6374ed1ec672ac874fa2bb54b
Vasant Balasubramanian <vasant.bala@gmail.com>
2023-06-25 10:56:48 -0500

    Kestrel Configuration to support cert auth

---
+++ ProjectAlpha/StandaloneApps/ProjectGamma/Configurations/AuthenticationSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectGamma.Configurations
+{
+    public class AuthenticationSettings
+    {
+        public string Mode { get; set; }
+        public string HostingType { get; set; }
+    }
+}
--- ProjectAlpha/StandaloneApps/ProjectGamma/Program.cs
+++ ProjectAlpha/StandaloneApps/ProjectGamma/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using ProjectGamma.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var authenticationSettings = new AuthenticationSettings();
+builder.Configuration.GetSection("AuthenticationSettings").Bind(authenticationSettings);
+
+if (authenticationSettings?.Mode == "Certificate")
+{
+    if (authenticationSettings.HostingType.Contains("Kestrel"))
+    {
+        builder.Services.Configure<KestrelServerOptions>(options =>
+        {
+            options.ConfigureHttpsDefaults(options =>
+                options.ClientCertificateMode = Microsoft.AspNetCore.Server.Kestrel.Https.ClientCertificateMode.RequireCertificate);
+        });
+    }
+}
+
+
 
 var app = builder.Build();
 
