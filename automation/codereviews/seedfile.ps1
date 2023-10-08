59250d01a57fd280ab7898a4ee0c2fc23fb2e620
vasantbala <30379760+vasantbala@users.noreply.github.com>
2023-10-07 22:21:18 -0500

    api

--- automation/codereviews/seedfile.ps1
+++ automation/codereviews/seedfile.ps1
@@ -7,5 +7,5 @@ param(
 
 Write-Host 'Hello world'
 Write-Host "Base: $($BaseCommitSha); Change: $($ChangeCommitSha)"
-$sha = git log -n 1 "refs/remotes/origin/main"
+$sha = gh api vasantbala/MultiProjectsCI/pulls/3
 Write-Host $sha
